ed64fbfcafb0857bf2fec8c63cb11c9150735ae3
Divya Prakash Pandey <pandeypaarul007@gmail.com>
2025-12-19 12:22:20 +0530

    Implement time complexity updates for search algorithms
    Added time complexity display for search algorithms.

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ document.getElementById("speed").addEventListener("input", (e) => {
   delay = Number(e.target.value);
   document.getElementById("speedValue").innerText = delay + " ms";
 });
+//Time COmplexiy
+function updateComplexity(algo) {
+  const complexityDiv = document.getElementById("complexity");
+
+  if (algo === "linear") {
+    complexityDiv.innerText = "Time Complexity: O(n)";
+  } else if (algo === "binary") {
+    complexityDiv.innerText = "Time Complexity: O(log n)";
+  }
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 
 function renderArray(arr, highlight = -1, found = -1) {
   const container = document.getElementById("array");
@@ -25,7 +36,7 @@ function renderArray(arr, highlight = -1, found = -1) {
   });
 }
 
-// ---------------- LINEAR SEARCH ----------------
+// Linear Search
 async function linearSearch(arr, target) {
   for (let i = 0; i < arr.length; i++) {
     renderArray(arr, i);
@@ -39,7 +50,7 @@ async function linearSearch(arr, target) {
   alert("Target not found");
 }
 
-// ---------------- BINARY SEARCH ----------------
+// Binary Search
 async function binarySearch(arr, target) {
   let l = 0, r = arr.length - 1;
 
@@ -59,7 +70,7 @@ async function binarySearch(arr, target) {
   alert("Target not found");
 }
 
-// ---------------- START ----------------
+// Start
 function start() {
   const arrInput = document.getElementById("arrayInput").value.trim();
   const target = Number(document.getElementById("targetInput").value);
@@ -82,3 +93,4 @@ function start() {
 }
 
 window.start = start;
+
